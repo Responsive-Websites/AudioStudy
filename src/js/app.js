c300d318f8987b38e48f07772f29c03d3044632e
Danila-hub <kontaigabox@mail.ru>
2022-05-26 17:00:39 +0300

    Added header and styles for it

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,12 @@
 //import * as flsFunctions from "./modules/functions.js";
 //flsFunctions.thisTest();
 
-
+const iconMenu = document.querySelector('.control-header__button-burger');
+if (iconMenu) {
+  const menuBody = document.querySelector('.control-header__buttons');
+  iconMenu.addEventListener('click', function (e) {
+    document.body.classList.toggle('lock');
+    iconMenu.classList.toggle('active');
+    menuBody.classList.toggle('active');
+  });
+}
